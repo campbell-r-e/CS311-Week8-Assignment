09ead5dc2a50572b7f92b1713eb9f80c6f027efa
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-23 15:46:47 -0400

    added images and modified css

--- week8/src/pages/index.tsx
+++ week8/src/pages/index.tsx
@@ -1,115 +1,81 @@
-import Image from "next/image";
-import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { useEffect, useState } from "react";
 
+
+// below from online resource
+type Books = {
+  title: string;
+  Author: string;
+  Publisher: string;
+  Id:string;
+  pic:string;
+
+
+ 
+};
+
+
+
+
+  // 
+  
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [Books, setBooks] = useState<Books[]>([]);
+  const [selectedBookId, setSelectedBookId] = useState<string | null>(null); // from online resource
+  
+  // above from online resource example 
+  useEffect(() => {
+    fetch('/data.json') //from online
+      .then((response) => response.json()) //from online 
+      .then((data) => setBooks(data['Books'])) // from online example
+   
+    
+   
+  }, []);
+  
+  
+  function buttons(BooksId:string){
+    setSelectedBookId(BooksId);
+   
+   
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    
+  }
+  
+  return (
+   
+    <div>
+      <h1 id="heading">Books List</h1>
+     <ul>
+        {Books.map((Books) => (
+          <li key={Books.Id} >
+            <div>
+            <ol>
+             <li id = "List0">{Books.title}</li>
+             {selectedBookId === Books.Id && <li id ="List1">Author:{Books.Author}</li>} 
+             {selectedBookId === Books.Id && <li id ="List2">Publisher:{Books.Publisher}</li>} 
+             
+             {selectedBookId === Books.Id && <img src={Books.pic} alt="Girl in a jacket" width="300" height="300"></img>} 
+            </ol>
+            <button id="button" onClick={() => buttons(Books.Id)}>
+              Click for more info about the book.
+    </button> 
+            
+            </div> 
+
+            <br></br>
+           
+          
+          
+           
+          </li>
+         
+        ))}
+      </ul>
+
     </div>
+    // modifed from example online 
+    // button modified from online resource
+    // {selectedBookId === Books.Id && <li>{Books.Author}</li>}  from online resource
   );
 }
